cae6a719337b1e5af31befa48861805782cfc037
meaghan jennings <meaghanljennings@gmail.com>
2021-04-14 15:11:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,9 @@ class Scraper
   @@all = []
 
 def scrape_index_page
+  doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
   
+  doc.css(". ")
 end
 
 def scrape_profile_page
