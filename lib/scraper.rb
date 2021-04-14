02c71768e9dd546f84fb8851135bbb42792cfff6
meaghan jennings <meaghanljennings@gmail.com>
2021-04-14 14:47:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,18 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
-class Scraper
+class Scraper 
+  attr_accessor :student 
+  @@all = []
 
-  def self.scrape_index_page(index_url)
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
+def scrape_index_page
+  
 end
 
+def scrape_profile_page
+  
+end 
+
+
+end 
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,27 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location
 
   @@all = []
 
-  def initialize(student_hash)
+  def initialize(hash)
+    @name = hash[:name]
+    @location = hash[:location]
+    hash = self 
+    @@all << self 
+  end
+    
+  def self.create_from_collection
     
   end
 
-  def self.create_from_collection(students_array)
-    
-  end
-
-  def add_student_attributes(attributes_hash)
+  def add_student_attributes
     
   end
 
   def self.all
-    
+    @@all 
   end
+  
 end
 
