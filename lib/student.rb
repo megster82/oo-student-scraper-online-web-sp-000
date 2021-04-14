4a687d87d913c88f33aa3a21ecdb0399796b169a
meaghan jennings <meaghanljennings@gmail.com>
2021-04-14 15:29:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,27 +1,36 @@
 class Student
 
-  attr_accessor :name, :location, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url, :scraper
 
   @@all = []
 
-  def initialize(hash)
-    @name = hash[:name]
-    @location = hash[:location]
-    hash = self 
-    @@all << self 
-  end
-    
-  def self.create_from_collection
-    
+  def initialize(student_hash)
+    #binding.pry
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    student_hash = self
+    @@all << self
+
   end
 
-  def add_student_attributes
-    
+  def self.create_from_collection(students_array)
+    students_array.each do |student|
+      student = self.new(student)
+    end
+
+  end
+
+  def add_student_attributes(attributes_hash)
+    #binding.pry
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
   def self.all
-    @@all 
+    @@all
   end
-  
 end
-
