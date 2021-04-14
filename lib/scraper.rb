6ef5ccdb1a895408ea17d83ecb5a63e9a7c238e9
meaghan jennings <meaghanljennings@gmail.com>
2021-04-14 15:50:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    page = Nokogiri::HTML(open(profile_url))
+    doc = Nokogiri::HTML(open(profile_url))
 
     student_page = {}
 
